5cbbbf6ebc979f8f5fcfd200e803069f8d53e49b
Jorge Suárez <jorgealejandrosuarez0807@gmail.com>
2025-02-04 10:40:39 -0400

    volvemos al inicio

--- src/ToDoCounter.js
+++
@@ -1,9 +0,0 @@
-import './ToDoCounter.css';
-
-function ToDoCounter({ total , completed}){
-  return(
-    <h1 className="title">Has completado <b>{completed}</b> de <b>{total}</b> To Do</h1>
-  );
-}
-
-export{ToDoCounter};
